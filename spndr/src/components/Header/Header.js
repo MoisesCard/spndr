ff225bae4ffce1ec425200236391246022234c6d
KingMo <macv123321@gmail.com>
2023-02-21 00:35:57 -0500

    started project and started with header

---
+++ spndr/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./header.css"
+
+
+const Header = () => {
+  return (
+   <div className='nav'>
+       <h1 className='title'>SPNDR</h1>
+       <button className='settings'>Setting</button>
+   </div>
+
+
+  )
+}
+
+export default Header
